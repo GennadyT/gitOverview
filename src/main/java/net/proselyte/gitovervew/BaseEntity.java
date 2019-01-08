0b6e35530b90343677d84ec25907109c36c6d8c6
Gennady9java <anubis.gen@gmail.com>
2019-01-08 14:50:18 +0200

    Adding new class BaseEntity

---
+++ src/main/java/net/proselyte/gitovervew/BaseEntity.java
@@ -0,0 +1,23 @@
+package net.proselyte.gitovervew;
+
+public class BaseEntity {
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+    public  boolean isNew(){
+       return (this.id == 0);
+    }
+}
